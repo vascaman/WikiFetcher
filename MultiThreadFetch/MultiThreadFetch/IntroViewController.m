49247fb34141b864042e10a379a8fce6b6d66773
vascaman <stefono@Julia.local>
2018-03-27 15:59:43 +0200

    new look and new intro view controller

--- MultiThreadFetch/MultiThreadFetch/IntroViewController.m
+++ MultiThreadFetch/MultiThreadFetch/IntroViewController.m
@@ -9,13 +9,83 @@
 #import "IntroViewController.h"
 
 @interface IntroViewController ()
-
+@property(nonatomic, retain)UITextView * introText;
+@property(nonatomic, retain)UIButton * dismissButton;
 @end
 
 @implementation IntroViewController
+@synthesize introText;
+@synthesize dismissButton;
+
+-(void)dealloc
+{
+    [introText release];
+    [dismissButton release];
+    [super dealloc];
+}
+
+-(UITextView*)introText
+{
+    if (!introText)
+    {
+        CGFloat padding = 20;
+        
+        CGRect textViewRect = self.view.bounds;
+        textViewRect.origin = CGPointMake(padding, padding);
+        textViewRect.size.width -= padding*2;
+        textViewRect.size.height -= padding*6;
+        
+        introText = [[UITextView alloc] initWithFrame:textViewRect];
+        [introText setAutoresizingMask:UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleRightMargin];
+        [introText setTextAlignment:NSTextAlignmentCenter];
+        [introText setEditable:NO];
+        NSURL * textUrl = [NSURL URLWithString:@"https://raw.githubusercontent.com/vascaman/WikiFetcher/master/README.md"];
+        
+        NSString * text = [NSString stringWithContentsOfURL:textUrl
+                                                   encoding:NSUTF8StringEncoding
+                                                      error:nil];
+        
+        [introText setText:text];
+        
+        
+        [introText.layer setBorderColor:[UIColor blueColor].CGColor];
+        [introText.layer setBorderWidth:2];
+    }
+    
+    return introText;
+}
+
+-(UIButton*)dismissButton
+{
+    if (!dismissButton)
+    {
+        dismissButton = [[UIButton alloc] initWithFrame:CGRectMake(10, 10, 200, 50)];
+        [dismissButton setTitle:@"Dismiss" forState:UIControlStateNormal];
+        [dismissButton addTarget:self
+                          action:@selector(dismiss)
+                forControlEvents:UIControlEventTouchUpInside];
+        [dismissButton setBackgroundColor:[UIColor redColor]];
+    }
+    return dismissButton;
+}
+
+-(void)dismiss
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+-(void)loadView
+{
+    [super loadView];
+    [self.view setBackgroundColor:[UIColor redColor]];
+    //[self.introText setBackgroundColor:[UIColor whiteColor]];
+    [self.view addSubview:self.introText];
+    [self.view addSubview:self.dismissButton];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     // Do any additional setup after loading the view.
 }
 
@@ -24,14 +94,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
--- MultiThreadFetch/MultiThreadFetch/ViewController.m
+++ MultiThreadFetch/MultiThreadFetch/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import "WikiPageFetcher.h"
 #import "ImageDetailViewController.h"
+//#import "IntroViewController.h"
+
 #define WIKI_IMAGE_CELL_ID @"myCellId"
 @interface ViewController ()
 @property (nonatomic, retain)WikiPageFetcher * pageFetcher;
@@ -16,6 +18,7 @@
 @property (nonatomic, retain)UIActivityIndicatorView * spinner;
 @property (nonatomic, retain)NSRecursiveLock * updateLock;
 @property (nonatomic, retain)UISearchBar * searchBar;
+@property (nonatomic, assign)BOOL introDone;
 @end
 
 @implementation ViewController
@@ -24,6 +27,7 @@
 @synthesize imagesTableView;
 @synthesize spinner;
 @synthesize updateLock;
+@synthesize introDone;
 
 -(void)dealloc
 {
@@ -121,11 +125,11 @@
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 
-    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:WIKI_IMAGE_CELL_ID];
+    AruCellView * cell = [tableView dequeueReusableCellWithIdentifier:WIKI_IMAGE_CELL_ID];
     
     if (!cell)
     {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+        cell = [[[AruCellView alloc] initWithStyle:UITableViewCellStyleSubtitle
                                       reuseIdentifier:WIKI_IMAGE_CELL_ID] autorelease];
     }
     
@@ -255,11 +259,31 @@
     [self.view addSubview:self.searchBar];
     [self.view addSubview:self.imagesTableView];
     [self.view addSubview:self.spinner];
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
 }
 
 -(void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
+    if (!self.introDone)
+    {
+//        IntroViewController * introVC = [[IntroViewController alloc] init];
+//        
+//        [self presentViewController:introVC
+//                           animated:YES
+//                         completion:nil];
+//        
+//        [introVC release];
+        self.introDone = YES;
+    }
+    
+    
 }
 
 - (void)viewDidLoad
@@ -275,6 +299,89 @@
 -(void)notifyUser:(NSString *)text
 {
     [ToastView makeToastWihtText:text];
+}
+
+@end
+
+#define CELL_PADDING 2
+
+@interface AruCellView()
+@property(nonatomic, retain)UIActivityIndicatorView * spinner;
+@end
+
+@implementation AruCellView
+@synthesize spinner;
+
+
+-(void)dealloc
+{
+    [spinner release];
+    [super dealloc];
+}
+
+-(UIActivityIndicatorView*)spinner
+{
+    if (!spinner)
+    {
+        spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    }
+    
+    return spinner;
+}
+
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    [self updateImageView];
+    [self updateTextLabel];
+    [self updateSeparator];
+    [self updateSpinner];
+}
+
+-(void)updateSeparator
+{
+    self.separatorInset = UIEdgeInsetsZero;
+}
+
+-(void)updateSpinner
+{
+    if (self.imageView.image)
+    {
+        [self.spinner removeFromSuperview];
+        return;
+    }else if(![self.spinner isAnimating])
+    {
+        [self addSubview:self.spinner];
+        [self.spinner startAnimating];
+        CGRect newRect = CGRectZero;
+        newRect.size.width = newRect.size.height = self.bounds.size.height - CELL_PADDING*2;
+        newRect.origin.x = newRect.origin.y = CELL_PADDING;
+        self.spinner.frame = newRect;
+    }
+}
+
+-(void)updateTextLabel
+{
+    CGRect newRect = self.textLabel.frame;
+    newRect.origin.x = self.imageView.frame.origin.x + self.imageView.frame.size.width + CELL_PADDING;
+    newRect.size.width = self.bounds.size.width - newRect.origin.x - CELL_PADDING;
+    [self.textLabel setFrame:newRect];
+    //[self.textLabel.layer setBorderWidth:2];
+    //[self.textLabel.layer setBorderColor:[UIColor blueColor].CGColor];
+    
+}
+
+-(void)updateImageView
+{
+    CGRect newRect = CGRectZero;
+    newRect.size.width = newRect.size.height = self.bounds.size.height - CELL_PADDING*2;
+    newRect.origin.x = newRect.origin.y = CELL_PADDING;
+    self.imageView.frame = newRect;
+    //[self.imageView.layer setBorderWidth:2];
+    //[self.imageView.layer setBorderColor:[UIColor redColor].CGColor];
+    [self.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [self.imageView setBackgroundColor:[UIColor colorWithWhite:1 alpha:0]];
 }
 
 @end
